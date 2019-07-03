0f239247a8db259db0df9a878f12b7539bc1ff63
xym760 <1455422076@qq.com>
2019-07-03 17:06:28 +0800

    spring boot基础篇2019/7/3

--- src/test/java/com/nxist/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/nxist/springboot/SpringbootApplicationTests.java
@@ -3,6 +3,8 @@ package com.nxist.springboot;
 import com.nxist.springboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -17,6 +19,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootApplicationTests {
 
+    //记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     Person person;
     @Autowired
@@ -30,7 +35,16 @@ public class SpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println(person);
+//        System.out.println(person);
+        //日志的级别
+        //由低到高：trace<debug<info<warn<error
+        //可以调整输出的日志级别，然后日志就会这这个级别集以上的级别生效
+        logger.trace("这是trace日志。。。");
+        logger.debug("这是debug日志。。。");
+        //spring默认给我们使用的是info级别的,没有指定级别，就用spring boot的默认级别；root级别
+        logger.info("这是info日志。。。");
+        logger.warn("这是warn日志。。。");
+        logger.error("这是error日志。。。");
     }
 
 }
