8fdf720558da3c3978f016186ef370c72253d21b
xym760 <1455422076@qq.com>
2019-07-04 18:06:46 +0800

    spring boot thyme2019/7/4

--- src/main/java/com/nxist/springboot/SpringbootApplication.java
+++ src/main/java/com/nxist/springboot/SpringbootApplication.java
@@ -2,7 +2,12 @@ package com.nxist.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 //@ImportResource(locations = {"classpath:beans.xml"})
 @SpringBootApplication
@@ -12,4 +17,16 @@ public class SpringbootApplication {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
+    @Bean
+    public ViewResolver myViewResolver(){
+        return new MyViewResolver();
+    }
+
+    private static class MyViewResolver implements ViewResolver{
+
+        @Override
+        public View resolveViewName(String viewName, Locale locale) throws Exception {
+            return null;
+        }
+    }
 }
--- src/main/java/com/nxist/springboot/controller/HelloController.java
+++ src/main/java/com/nxist/springboot/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.nxist.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Map;
 
 /**
  * @Author: xym760
@@ -9,12 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 //@ResponseBody//将数据直接返回给浏览器
-//@Controller
-@RestController
+@Controller
+//@RestController
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World quick!";
+    }
+
+    //查出一些数据在页面展示
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello", "<h1>你好！</h1>");
+        map.put("users", Arrays.asList("zhangshan", "lisi", "wangwu"));
+        return "success";
     }
 }
